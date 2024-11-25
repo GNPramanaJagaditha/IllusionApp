d506b9b4928d4b4cf0b8a41005cc59229562a01f
GNPramanaJagaditha <jgdtpram@gmail.com>
2024-11-25 22:08:29 +0700

    Add register page layout

---
+++ app/src/main/java/com/example/illusionapp/view/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.illusionapp.view.register
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.illusionapp.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
